21fd829c5ad2876b25076e93cb79fd90134412b7
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2025-02-03 12:53:54 +0000

    feat: implementação de componentes

--- projeto/src/app/app.component.ts
+++ projeto/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProdutosComponent } from './componentes/produtos/produtos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProdutosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- projeto/src/app/componentes/produtos/produtos.component.ts
+++ projeto/src/app/componentes/produtos/produtos.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-produtos',
-  imports: [],
+  imports: [
+    InputTextModule
+  ],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
